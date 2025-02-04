104f5e9f2e6a01be1522e49b92af18bdad8f3dc4
ZhuChongLin <8210856@qq.com>
2025-02-04 14:59:15 +0800

    build(package): 优化打包配置并更新类型定义
    - 更新 package.json 中的入口文件路径
    - 修改 tsconfig.json，启用声明文件生成
    - 调整 vite.config.ts，优化打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
 import { defineConfig } from 'vite'
-import dtsPlugin from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [
-    dtsPlugin({
-      insertTypesEntry: true,
-      include: ['src'],
-      rollupTypes: true
-    })
-  ],
   esbuild: {
     minifyIdentifiers: false // 禁止压缩标识符（类名、变量名等）
   },
   build: {
-    outDir: 'dist',
     lib: {
       entry: 'src/index.ts',
       name: 'CssInJs',
       fileName: (format) => `css-in-js.${format}.js`,
-      formats: ['es', 'umd']
+      formats: ['umd']
     },
     rollupOptions: {
       external: ['vitarx'],
