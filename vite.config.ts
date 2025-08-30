a7bacb14a755b22d52f806dedfb67a515217a368
ZhuChongLin <8210856@qq.com>
2025-08-30 15:00:51 +0800

    build(vite): 添加类型定义插件并调整打包格式
    - 添加 vite-plugin-dts 插件，用于生成类型定义文件
    - 调整打包格式，从 ['umd'] 改为 ['es', 'iife']
    - 更新 rollupOptions，设置 'vitarx' 为外部依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
+import dtsPlugin from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [
+    dtsPlugin({
+      include: ['src'],
+      insertTypesEntry: true,
+      rollupTypes: true
+    })
+  ],
   esbuild: {
     minifyIdentifiers: false // 禁止压缩标识符（类名、变量名等）
   },
@@ -9,7 +17,7 @@ export default defineConfig({
       entry: 'src/index.ts',
       name: 'CssInJs',
       fileName: (format) => `css-in-js.${format}.js`,
-      formats: ['umd']
+      formats: ['es', 'iife']
     },
     rollupOptions: {
       external: ['vitarx'],
