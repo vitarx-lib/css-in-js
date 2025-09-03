d288365c6024d566a4be0bfa6be7943067322b79
ZhuChongLin <8210856@qq.com>
2025-09-03 15:29:54 +0800

    build(vite): 更新打包配置并移除类型定义插件
    - 移除了 vite-plugin-dts插件
    - 修改了打包输出文件名和格式
    - 增加了 emptyOutDir 配置项
    - 简化了 rollupOptions 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,14 @@
 import { defineConfig } from 'vite'
-import dtsPlugin from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [
-    dtsPlugin({
-      include: ['src'],
-      insertTypesEntry: true,
-      rollupTypes: true
-    })
-  ],
-  esbuild: {
-    minifyIdentifiers: false // 禁止压缩标识符（类名、变量名等）
-  },
   build: {
     lib: {
       entry: 'src/index.ts',
       name: 'CssInJs',
-      fileName: (format) => `css-in-js.${format}.js`,
-      formats: ['es', 'iife']
+      fileName: (format) => `index.${format}.js`,
+      formats: ['iife']
     },
+    emptyOutDir: false,
     rollupOptions: {
       external: ['vitarx'],
       output: {
